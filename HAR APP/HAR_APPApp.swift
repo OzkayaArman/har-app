c6a6193dd7ef476f9dc1ac80a41f503bfa5c8fcf
ArmanOzkaya <ao85@st-andrews.ac.uk>
2024-10-01 22:24:52 +0100

    Initial Commit

---
+++ HAR APP/HAR_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAR_APPApp.swift
+//  HAR APP
+//
+//  Created by Arman Ozkaya on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HAR_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HAR APPTests/HAR_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  HAR_APPTests.swift
+//  HAR APPTests
+//
+//  Created by Arman Ozkaya on 01/10/2024.
+//
+
+import Testing
+@testable import HAR_APP
+
+struct HAR_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
