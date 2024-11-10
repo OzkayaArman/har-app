f2e0d2b6b39aee9d5059ffdc402a41d324880c57
ArmanOzkaya <ao85@st-andrews.ac.uk>
2024-11-10 17:53:12 +0000

    Added account page

---
+++ HAR APP/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  HAR APP
+//
+//  Created by Arman Ozkaya on 08/11/2024.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ HAR APP/SetingsView.swift
@@ -0,0 +1,18 @@
+//
+//  Setings.swift
+//  HAR APP
+//
+//  Created by Arman Ozkaya on 08/11/2024.
+//
+
+import SwiftUI
+
+struct SetingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetingsView()
+}
